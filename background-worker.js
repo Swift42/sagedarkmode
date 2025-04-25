280a49b0aba5394a09b4c31ea601b946dced77ca
Swift42 <1180176+Swift42@users.noreply.github.com>
2025-04-25 02:59:41 +0200

    Update background-worker.js

--- background-worker.js
+++ background-worker.js
@@ -1,13 +1,1 @@
-const newRules = [];
-newRules.push({ "id": 1, "priority": 1, 
-	"action": { "type": "redirect", "redirect": { "url": chrome.runtime.getURL("labs-background.jpg") } } ,
-    "condition": { "urlFilter": "https://cdn.staratlas.com/sage-labs/labs-background.jpeg", "resourceTypes": ["image"] }
-});	
-
-chrome.declarativeNetRequest.getDynamicRules(previousRules => {
-  const previousRuleIds = previousRules.map(rule => rule.id);
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: previousRuleIds,
-    addRules: newRules
-  });
-});
+ 
